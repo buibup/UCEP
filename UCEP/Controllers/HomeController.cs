fd4abc2af6158004f8b1bb8e450ac09d39c4f0e6
buibup <b5209194@gmail.com>
2017-07-18 17:12:00 +0700

    18072017

--- UCEP/Controllers/HomeController.cs
+++ UCEP/Controllers/HomeController.cs
@@ -1,30 +1,36 @@
-﻿using System;
+﻿using LINQtoCSV;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UCEP.Models;
 
 namespace UCEP.Controllers
 {
     public class HomeController : Controller
     {
+        private UCEPDbContext db = new UCEPDbContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult UploadCsv(HttpPostedFileBase attachmentcsv)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            CsvFileDescription csvFileDescription = new CsvFileDescription
+            {
+                SeparatorChar = ',',
+                FirstLineHasColumnNames = true
+            };
+            CsvContext csvContext = new CsvContext();
+            StreamReader streamReader = new StreamReader(attachmentcsv.InputStream);
+            IEnumerable<FsCatalogue> list = csvContext.Read<FsCatalogue>(streamReader, csvFileDescription);
+            db.FsCatalogue.AddRange(list);
+            db.SaveChanges();
+            return Redirect("GetAllEmployeeData");
         }
     }
 }
