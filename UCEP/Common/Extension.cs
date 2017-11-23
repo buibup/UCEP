e1878512935cd16fc9b65ed5cae83b78bfb7b6b1
mongkol.me <mongkol.me@glsict.com>
2017-11-23 17:03:44 +0700

    23112017

--- UCEP/Common/Extension.cs
+++ UCEP/Common/Extension.cs
@@ -1,21 +1,24 @@
-﻿using System;
+using System;
 using System.Data;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace PatientEmpathy.Common
 {
-    public static class Extension
+  public static class Extension
+  {
+    public static void AddInputParameters<T>(this IDbCommand cmd,
+             T parameters) where T : class
     {
-        public static void AddInputParameters<T>(this IDbCommand cmd,
-                 T parameters) where T : class
-        {
-            foreach (var prop in parameters.GetType().GetProperties())
-            {
-                object val = prop.GetValue(parameters, null);
-                var p = cmd.CreateParameter();
-                p.ParameterName = prop.Name;
-                p.Value = val ?? DBNull.Value;
-                cmd.Parameters.Add(p);
-            }
-        }
+      foreach (var prop in parameters.GetType().GetProperties())
+      {
+        object val = prop.GetValue(parameters, null);
+        var p = cmd.CreateParameter();
+        p.ParameterName = prop.Name;
+        p.Value = val ?? DBNull.Value;
+        cmd.Parameters.Add(p);
+      }
     }
+  }
 }
